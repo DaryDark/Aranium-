526c32a93f5749c4ec22e2f99637fba242dc1c89
DaryDark <daryusscraciun@gmail.com>
2026-05-24 20:09:44 +0200

    Remove Phantom SDK

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,11 @@
-import "react-native-get-random-values";
 import { Stack } from "expo-router";
-import { PhantomProvider, AddressType, darkTheme } from "@phantom/react-native-sdk";
 
-export default function Layout() {
+export default function RootLayout() {
   return (
-    <PhantomProvider
-      config={{
-        providers: ["google", "apple"],
-        appId: "3f772481-ad8b-413b-a894-732a8bfadb57",
-        scheme: "aranium",
-        addressTypes: [AddressType.solana],
-        authOptions: {
-          redirectUrl: "aranium://phantom-auth-callback",
-        },
+    <Stack
+      screenOptions={{
+        headerShown: false,
       }}
-      theme={darkTheme}
-      appName="ARAN"
-    >
-      <Stack screenOptions={{ headerShown: false }} />
-    </PhantomProvider>
+    />
   );
 }
